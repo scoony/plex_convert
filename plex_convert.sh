67fc8d49ba51b7b6893aadb24cbfd38fa1f4935b
Scoony <scoonydeus@gmail.com>
2022-12-19 22:58:18 +0100

    Fix

--- plex_convert.sh
+++ plex_convert.sh
@@ -108,6 +108,7 @@ while getopts eushf:cm:l:-: OPT; do
                   echo "The script is up to date... exit"
                 fi
               else
+                echo ""
                 echo "Script offline"
               fi
             else
